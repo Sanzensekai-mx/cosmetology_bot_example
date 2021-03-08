0cc0e787f3eb597b27fdd7b81fd5cc73a2932981
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-03-08 22:00:52 +0400

    Update config.py

--- data/config.py
+++ data/config.py
@@ -15,6 +15,7 @@ masters_id = str(os.environ.get('masters_id')).split(', ')   # Dashboard
 # ip = str(os.environ.get("ip"))
 
 # Ссылка подключения к базе данных
-POSTGRES_URI = f"postgresql://{PG_USER}:{PG_PASS}@{host}/{DATABASE}" # Dashboard
+POSTGRES_URI = str(os.environ.get('DATABASE_URL'))
+# POSTGRES_URI = f"postgresql://{PG_USER}:{PG_PASS}@{host}/{DATABASE}" # Dashboard
 # masters_and_id = {m_name: m_id for m_name in masters_username for m_id in masters_id}
 # masters_and_id = dict(zip(masters_username, masters_id))
