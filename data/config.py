0f2a3a9ff9d2d3b155ec40543056f2ed2c6f0912
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-03-08 21:53:25 +0400

    Update config.py for Heroku

--- data/config.py
+++ data/config.py
@@ -1,16 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(encoding='utf-8')
-BOT_TOKEN = os.getenv('BOT_TOKEN')  # Dashboard
-admins = str(os.getenv('admins')).split(', ')   # Dashboard
-masters_id = str(os.getenv('masters_id')).split(', ')   # Dashboard
+# load_dotenv(encoding='utf-8')
+BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))  # Dashboard
+admins = str(os.environ.get('admins')).split(', ')   # Dashboard
+masters_id = str(os.environ.get('masters_id')).split(', ')   # Dashboard
 # masters_username = str(os.getenv("masters_username")).split(', ')
 
-host = str(os.getenv("PG_HOST"))  # хост базы данных
-PG_USER = str(os.getenv("PG_USER"))  # имя владельца базы данных
-PG_PASS = str(os.getenv("PG_PASS"))  # пароль бд
-DATABASE = str(os.getenv("DATABASE"))  # имя БД в pgAdmin
+# host = str(os.getenv("PG_HOST"))  # хост базы данных
+# PG_USER = str(os.getenv("PG_USER"))  # имя владельца базы данных
+# PG_PASS = str(os.getenv("PG_PASS"))  # пароль бд
+# DATABASE = str(os.getenv("DATABASE"))  # имя БД в pgAdmin
 
 # ip = str(os.environ.get("ip"))
 
