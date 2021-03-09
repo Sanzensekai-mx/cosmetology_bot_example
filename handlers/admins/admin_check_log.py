b9e81fe28723c3a261026d652ed461ce5e6906b9
Sanzensekai-mx <ivanymn@yandex.ru>
2021-03-09 20:24:25 +0400

    Тест изменения локали с помощью модуля locale x100500

--- handlers/admins/admin_check_log.py
+++ handlers/admins/admin_check_log.py
@@ -40,7 +40,7 @@ async def show_time(message: Message):
                          f'\nmaybe current'
                          f'\n{datetime.datetime.now(tz)}')
     # locale.setlocale(locale.LC_ALL, '')
-    locale.setlocale(locale.LC_ALL, ('ru_RU', 'UTF-8'))
+    locale.setlocale(locale.LC_ALL, 'ru_RU')
     c = calendar.TextCalendar(calendar.MONDAY)
     print_month_c = c.formatmonth(current_date.year, current_date.month)
     await message.answer(print_month_c)
