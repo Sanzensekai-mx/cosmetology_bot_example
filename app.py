4b5f41eb40b467a0899589c90cd332a0dcfd89c4
Sanzensekai-mx <ivanymn@yandex.ru>
2021-03-14 18:35:02 +0400

    Попытка не удалась

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import asyncio
 from utils.db_api.database import create_db
 from utils.del_old_logs import del_old_logs
-from utils.send_request import send_request
 
 
 async def on_startup(dp):
@@ -13,8 +12,6 @@ async def on_startup(dp):
     await on_startup_notify(dp)
     # Создание задачи на удаление старых записей
     asyncio.create_task(del_old_logs())
-    # Задача на отправку запроса каждые 5 минут
-    asyncio.create_task(send_request())
     await create_db()
 
 if __name__ == '__main__':
