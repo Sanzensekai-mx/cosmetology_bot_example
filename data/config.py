cc003f8b2365dce156781f68a1905346e3052be7
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2022-01-11 12:58:12 +0400

    Deploy version of config

--- data/config.py
+++ data/config.py
@@ -4,18 +4,18 @@ import calendar
 # import pytz
 from dotenv import load_dotenv
 
-load_dotenv(encoding='utf-8')
+# load_dotenv(encoding='utf-8')
 
-# BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))  # Dashboard
-BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
-# admins = str(os.environ.get('admins')).split(', ')   # Dashboard
-admins = str(os.getenv("admins")).split(', ')
-# masters_id = str(os.environ.get('masters_id')).split(', ')   # Dashboard
-masters_id = str(os.getenv("masters_id")).split(',')
-# masters_username = str(os.getenv("masters_username")).split(', ')
+BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))  # Dashboard
+# BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+admins = str(os.environ.get('admins')).split(', ')   # Dashboard
+# admins = str(os.getenv("admins")).split(', ')
+masters_id = str(os.environ.get('masters_id')).split(', ')   # Dashboard
+# masters_id = str(os.getenv("masters_id")).split(',')
 
 # Ссылка подключения к базе данных
 POSTGRES_URI = str(os.environ.get('DATABASE_URL'))
+# POSTGRES_URI = str(os.getenv("DATABASE_URL"))
 # masters_and_id = {m_name: m_id for m_name in masters_username for m_id in masters_id}
 # masters_and_id = dict(zip(masters_username, masters_id))
 rus_months = ['', 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
